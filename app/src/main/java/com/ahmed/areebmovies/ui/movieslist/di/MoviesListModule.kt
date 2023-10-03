79bb2ac625bfb9c7f01d086bbf4ea12479db5f40
Ahmed Alaa <ahmedalaahussein00@gmail.com>
2023-10-03 20:18:44 +0300

    Add movies list flow

---
+++ app/src/main/java/com/ahmed/areebmovies/ui/movieslist/di/MoviesListModule.kt
@@ -0,0 +1,47 @@
+package com.ahmed.areebmovies.ui.movieslist.di
+
+import com.ahmed.areebmovies.data.local.ILocalDataSource
+import com.ahmed.areebmovies.data.remote.IRemoteDataSource
+import com.ahmed.areebmovies.data.repositories.movieslist.GetMoviesListRepository
+import com.ahmed.areebmovies.data.repositories.movieslist.IGetMoviesListRepository
+import com.ahmed.areebmovies.data.shared_prefrences.IPreferencesDataSource
+import com.ahmed.areebmovies.domain.usecases.movieslist.IMoviesListUseCase
+import com.ahmed.areebmovies.domain.usecases.movieslist.MoviesListUseCase
+import com.ahmed.areebmovies.retrofit.RetrofitModule
+import com.ahmed.areebmovies.utils.connection_utils.IConnectionUtils
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module(includes = [RetrofitModule::class])
+@InstallIn(SingletonComponent::class)
+abstract class MoviesListModule {
+
+    companion object{
+        @Singleton
+        @Provides
+        fun provideMoviesListingRepository(
+            connectionUtils: IConnectionUtils,
+            mIRemoteDataSource: IRemoteDataSource,
+            mILocalDataSource: ILocalDataSource,
+            mIPreferencesDataSource: IPreferencesDataSource
+        ): IGetMoviesListRepository {
+            return GetMoviesListRepository(
+                connectionUtils,
+                mIRemoteDataSource,
+                mILocalDataSource,
+                mIPreferencesDataSource
+            )
+        }
+    }
+
+    @Singleton
+    @Binds
+    abstract fun bindIMoviesListUseCase(moviesUseCase: MoviesListUseCase): IMoviesListUseCase
+
+
+}
